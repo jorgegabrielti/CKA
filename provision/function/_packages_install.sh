4e6f72ed9695399bdbd5ae43c86fda0acef4970d
Jorge Gabriel <jorgegabriel.ti@gmail.com>
2024-05-28 20:38:52 -0300

    [update] general improvements

--- provision/function/_packages_install.sh
+++ provision/function/_packages_install.sh
@@ -1,10 +1,12 @@
 # Test: [OK]
-_packages_install () { 
+_packages_install () {
+
   echo -e "\e[40;32;1m[TASK]: packages_install\e[m\n"
-  for ((c=0 ; c<=$(wc -l conf/packages.txt | awk '{print $1}'); c++))
+  for ((c=0 ; c<=$(wc -l ${WORK_DIR}/provision/conf/packages.txt | awk '{print $1}'); c++))
   do 
-    PACKAGE[$c]="$(cat conf/packages.txt | head -n$(($c + 1)) | tail -n1)"
+    PACKAGE[$c]="$(cat ${WORK_DIR}/provision/conf/packages.txt | head -n$(($c + 1)) | tail -n1)"
   done
   sudo apt update 
-  sudo snap install ${PACKAGE[*]}
+  sudo apt install -y ${PACKAGE[*]}
+
 }
--- provision/provision.sh
+++ provision/provision.sh
@@ -24,6 +24,12 @@ shopt -s expand_aliases
 ### Workdir
 WORK_DIR="/vagrant"
 
+### Requirements
+# yq
+yq --version && echo "yq already install." \
+|| wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
+    chmod +x /usr/bin/yq
+
 ### Import functions
 for FUNCTION in $(grep -F 'Test: [OK]' -l -r ${WORK_DIR}/provision/function/); do
   sed -i 's/\r$//' ${FUNCTION}
@@ -33,6 +39,8 @@ done
 ### Distro Detect
 _distro_detect
 
+
+
 ### Packages installations
 _packages_install
 
@@ -40,12 +48,12 @@ _packages_install
 echo -e "\e[40;32;1m[TASK]: Kubernetes Component Detect\e[m\n"
 
 # Get count total objects in /vagrant/conf/box_configs.yaml
-BOX_CONFIGS=$(yq '.[].k8s_component' < /vagrant/conf/box_configs.yaml | wc -l)
+BOX_CONFIGS=$(yq e '.[].kubernetes_component' - < ${WORK_DIR}/conf/box_configs.yaml | wc -l)
 
 for ((i=0; i<${BOX_CONFIGS}; i++)) 
 do 
-  yq ".[$i]" < /vagrant/conf/box_configs.yaml | grep -E "${HOSTNAME}" > /dev/null \
-  && KUBERNETES_COMPONENT=$(yq ".[$i].k8s_component" < /vagrant/conf/box_configs.yaml)
+  yq e ".[$i]" - < /vagrant/conf/box_configs.yaml | grep -E "${HOSTNAME}" > /dev/null \
+  && KUBERNETES_COMPONENT=$(yq e ".[$i].kubernetes_component" - < /vagrant/conf/box_configs.yaml)
 done
 
 case ${KUBERNETES_COMPONENT} in
